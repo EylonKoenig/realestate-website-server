8aba527e975beda8435e3c97968cfe2c1c892945
EylonKoenig <eylonkoenigm@walla.com>
2020-02-28 14:46:08 +0200

    SET DOMAIN

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ router.post('/', function(req, res, next) {
         .then(user => {
             if (user) {
                 const UserDetails = { id: user.id, user: user.email, password: user.password, first_name: user.first_name, role_id: user.role_id }
-                res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, httpOnly: false, domain: "koenigrealestate.netlify.com" })
+                res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, httpOnly: false })
                 res.status(200).send({ user })
             } else { res.status(401).json({ error: "worng user or password" }) }
         })
