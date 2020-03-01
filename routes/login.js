32dda910474c6a9b8e5b8b32bb004c89b4c849bb
EylonKoenig <eylonkoenigm@walla.com>
2020-03-01 17:10:02 +0200

    send auth

--- routes/login.js
+++ routes/login.js
@@ -9,8 +9,8 @@ router.post('/', function(req, res, next) {
         .then(user => {
             if (user) {
                 const UserDetails = { id: user.id, user: user.email, password: user.password, first_name: user.first_name, role_id: user.role_id }
-                res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, httpOnly: false })
-                res.status(200).send({ user })
+                    // res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, httpOnly: false })
+                res.status(200).send(JSON.stringify(UserDetails))
             } else { res.status(401).json({ error: "worng user or password" }) }
         })
         .catch(error => res.status(401).json({ error: error.message }));
