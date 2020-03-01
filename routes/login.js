82706d00413a0ac6af3623dadaf6345838c5cbf6
EylonKoenig <eylonkoenigm@walla.com>
2020-03-01 16:26:31 +0200

    cookie

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
             if (user) {
                 const UserDetails = { id: user.id, user: user.email, password: user.password, first_name: user.first_name, role_id: user.role_id }
                 console.log(document.cookie)
-                res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, domain: ".herokuapp.com", sameSite: "none", secure })
+                res.cookie("auth", JSON.stringify(UserDetails), { maxAge: 1000 * 60 * 60 * 24, domain: ".herokuapp.com", sameSite: "none", })
                 res.status(200).send({ user })
             } else { res.status(401).json({ error: "worng user or password" }) }
         })
